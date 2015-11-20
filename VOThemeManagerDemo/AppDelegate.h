70bbaae8b3d62392347a28e32181d24d7f092e3b
pozi119 <pozi119@163.com>
2015-11-20 21:45:30 +0800

    Initial Commit

---
+++ VOThemeManagerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VOThemeManagerDemo
+//
+//  Created by Valo on 15/11/20.
+//  Copyright © 2015年 Valo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VOThemeManagerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VOThemeManagerDemo
+//
+//  Created by Valo on 15/11/20.
+//  Copyright © 2015年 Valo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
